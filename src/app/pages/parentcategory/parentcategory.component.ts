107c2475b5d31896d874b95fadbe4ddf89225edd
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-06-30 12:19:22 +0200

    init: init project

---
+++ src/app/pages/parentcategory/parentcategory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parentcategory',
+  standalone: true,
+  imports: [],
+  templateUrl: './parentcategory.component.html',
+  styleUrl: './parentcategory.component.css',
+})
+export class ParentcategoryComponent {}
